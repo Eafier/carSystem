c60691d2b33a0f4dcc6423183a9c9d6b7b1465c1
s4cf7i <yifei_xin@saic-gm.com>
2020-05-18 20:42:24 +0800

    两个接口

--- src/main/java/com/jkxy/car/api/controller/CarController.java
+++ src/main/java/com/jkxy/car/api/controller/CarController.java
@@ -6,8 +6,8 @@ import com.jkxy.car.api.utils.JSONResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
-
 
 
 @RestController
@@ -85,4 +85,48 @@ public class CarController {
         carService.insertCar(car);
         return JSONResult.ok();
     }
+
+    /**
+     * 通过车名和型号购车
+     * 每辆车在数据库中为一条数据，车名和型号共同决定买车类型
+     *
+     * @param carName
+     * @param carType
+     * @return
+     */
+    @GetMapping("buyCar/{carName}/{carType}/{number}")
+    public JSONResult findByCarName(@PathVariable String carName, @PathVariable String carType, @PathVariable Integer number) {
+        //查询某品牌型号车的list
+        List<Car> cars = carService.findByCarNameAndType(carName, carType);
+        //是否满足购买数量
+        if (number > cars.size()) {
+            return JSONResult.ok("购车数超出剩余数量");
+        }
+        //删除被购买车辆数据
+        for (int i = 0; i < number; i++) {
+            carService.deleteById(cars.get(i).getId());
+        }
+        return JSONResult.ok("购车成功");
+    }
+
+    /**
+     * 模糊查询,获取范围数据
+     *
+     */
+    @GetMapping("findFromTo/{first}/{second}")
+    public JSONResult findFromTo(@PathVariable Integer first, @PathVariable Integer second) {
+        //传参要求
+        if (first < 0 || first > second) {
+            return JSONResult.ok("参数非法");
+        }
+        List<Car> cars = carService.findAll();
+        List<Car> range = new ArrayList<>();
+        for (int i = 0; i < cars.size(); i++) {
+            if (i >= first-1 && i <= second-1) {
+                range.add(cars.get(i));
+            }
+        }
+        return JSONResult.ok(range);
+    }
+
 }
